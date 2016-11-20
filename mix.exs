7b2ac618b223a693b1cb7c31dbbbad334565e36c
Allen Madsen <blatyo@gmail.com>
2016-11-19 22:32:53 -0500

    Add license

--- mix.exs
+++ mix.exs
@@ -44,7 +44,7 @@ defmodule Conduit.Mixfile do
   defp package do
     [# These are the default files included in the package
      name: :conduit,
-     files: ["lib", "priv", "mix.exs", "README*", "readme*", "LICENSE*", "license*"],
+     files: ["lib", "mix.exs", "README*", "LICENSE*"],
      maintainers: ["Allen Madsen"],
      licenses: ["Apache 2.0"],
      links: %{"GitHub" => "https://github.com/conduitframework/conduit",
