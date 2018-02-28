3276ec8dc1cfefefd5e6a13c85bce6610ac6006c
Allen Madsen <blatyo@gmail.com>
2018-02-27 22:43:45 -0500

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Conduit.Mixfile do
   def project do
     [
       app: :conduit,
-      version: "0.10.5",
+      version: "0.10.6",
       elixir: "~> 1.3",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
